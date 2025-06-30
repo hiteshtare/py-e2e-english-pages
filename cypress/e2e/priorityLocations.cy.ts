5761b5146f5bf28e6b79fe341acdd9a6e7a0ff9e
Hitesh Tare <hiteshtare@gmail.com>
2025-06-30 16:14:30 +0530

    Pointed to Staging2

--- cypress/e2e/priorityLocations.cy.ts
+++ cypress/e2e/priorityLocations.cy.ts
@@ -4,6 +4,20 @@ import { captureSreenshot } from "./util/common.util";
 
 const testDataForItems = require("../fixtures/locations.json");
 
+beforeEach(function () {
+  // Define and Encode the authentication string
+  const authString = `yssdev:Jaiguru@123!`;
+  const encodedAuth = Buffer.from(authString).toString("base64");
+
+  // Preparing to intercept the /basic_auth request and add the auth header once we click on login
+  cy.intercept("GET", "**", (req) => {
+    req.headers["authorization"] = `Basic ${encodedAuth}`;
+  }).as("basicAuth");
+  cy.intercept("POST", "**", (req) => {
+    req.headers["authorization"] = `Basic ${encodedAuth}`;
+  }).as("basicAuth");
+});
+
 describe("Locations", () => {
   testDataForItems.forEach((testCase: any, index: number) => {
     it(`#${index} page ${testCase.label} should have Page Load Time less than ${TEST_CONFIG.maximumPageLoadTime} seconds`, () => {
@@ -13,11 +27,28 @@ describe("Locations", () => {
           "navigation"
         ) as PerformanceNavigationTiming[];
         if (navigationTiming) {
-          const pageLoadTime = +(
+          const pageLoadTime = + (
             (navigationTiming.loadEventEnd - navigationTiming.startTime) /
             1000
           ).toFixed(4);
+          const ttfb =
+            navigationTiming.responseStart - navigationTiming.startTime;
+          const fcp =
+            navigationTiming.domContentLoadedEventEnd -
+            navigationTiming.startTime;
+          const responseTime =
+            navigationTiming.responseEnd - navigationTiming.requestStart;
+          const throughput =
+            navigationTiming.encodedBodySize /
+              (navigationTiming.responseEnd - navigationTiming.responseStart) ||
+            0;
+          
           cy.log(`Total Load Time: ${pageLoadTime} s`);
+          cy.log(`Time to First Byte: ${ttfb} ms`);
+          cy.log(`First Contentful Paint: ${fcp} ms`);
+          cy.log(`Response Time: ${responseTime} ms`);
+          cy.log(`Throughput: ${throughput}`);
+          
           expect(pageLoadTime).to.be.lessThan(TEST_CONFIG.maximumPageLoadTime);
         }
       });
--- cypress/e2e/priorityPages.cy.ts
+++ cypress/e2e/priorityPages.cy.ts
@@ -5,24 +5,61 @@ import { captureSreenshot } from "./util/common.util";
 // using baseURL
 const testDataForItems = require("../fixtures/priority-pages.json");
 
-// const baseUrl = Cypress.config("baseUrl");
+beforeEach(function () {
+  // Define and Encode the authentication string
+  const authString = `yssdev:Jaiguru@123!`;
+  const encodedAuth = Buffer.from(authString).toString("base64");
 
-// beforeEach(function () {
-//   // Define and Encode the authentication string
-//   const authString = `yssdev:Jaiguru@123!`;
-//   const encodedAuth = Buffer.from(authString).toString("base64");
-//   const authorizedMsg = "Congratulations! You must have the proper credentials";
-
-//   // Preparing to intercept the /basic_auth request and add the auth header once we click on login
-//   cy.intercept("GET", "**", (req) => {
-//     req.headers["authorization"] = `Basic ${encodedAuth}`;
-//   }).as("basicAuth");
-//   cy.intercept("POST", "**", (req) => {
-//     req.headers["authorization"] = `Basic ${encodedAuth}`;
-//   }).as("basicAuth");
-// });
+  // Preparing to intercept the /basic_auth request and add the auth header once we click on login
+  cy.intercept("GET", "**", (req) => {
+    req.headers["authorization"] = `Basic ${encodedAuth}`;
+  }).as("basicAuth");
+  cy.intercept("POST", "**", (req) => {
+    req.headers["authorization"] = `Basic ${encodedAuth}`;
+  }).as("basicAuth");
+});
 
 describe("Priority Pages", () => {
+
+  testDataForItems.forEach((testCase: any, index: number) => {
+     it(`#${index} page ${
+      testCase.label
+    } should have Page Load Time less than ${TEST_CONFIG.maximumPageLoadTime} seconds`, () => {
+      cy.visit(`${testCase.url}`);
+      cy.window().then((win) => {
+        const [navigationTiming] = win.performance.getEntriesByType(
+          "navigation"
+        ) as PerformanceNavigationTiming[];
+        if (navigationTiming) {
+          const pageLoadTime = + (
+            (navigationTiming.loadEventEnd - navigationTiming.startTime) /
+            1000
+          ).toFixed(4);
+          const ttfb =
+            navigationTiming.responseStart - navigationTiming.startTime;
+          const fcp =
+            navigationTiming.domContentLoadedEventEnd -
+            navigationTiming.startTime;
+          const responseTime =
+            navigationTiming.responseEnd - navigationTiming.requestStart;
+          const throughput =
+            navigationTiming.encodedBodySize /
+              (navigationTiming.responseEnd - navigationTiming.responseStart) ||
+            0;
+          
+          cy.log(`Total Load Time: ${pageLoadTime} s`);
+          cy.log(`Time to First Byte: ${ttfb} ms`);
+          cy.log(`First Contentful Paint: ${fcp} ms`);
+          cy.log(`Response Time: ${responseTime} ms`);
+          cy.log(`Throughput: ${throughput}`);
+          
+          expect(pageLoadTime).to.be.lessThan(TEST_CONFIG.maximumPageLoadTime);
+        }
+      });
+      captureSreenshot();
+    });
+  });
+
   // it(`#${1} page link: should have Page Load Time less than 1 second`, () => {
   //   // cy.visit(`/event/sadhana-sangams`);
 
@@ -57,26 +94,4 @@ describe("Priority Pages", () => {
   //   });
   //   captureSreenshot();
   // });
-
-  testDataForItems.forEach((testCase: any, index: number) => {
-    it(`#${index} page ${
-      testCase.label
-    } should have Page Load Time less than ${TEST_CONFIG.maximumPageLoadTime} seconds`, () => {
-      cy.visit(`${testCase.url}`);
-      cy.window().then((win) => {
-        const [navigationTiming] = win.performance.getEntriesByType(
-          "navigation"
-        ) as PerformanceNavigationTiming[];
-        if (navigationTiming) {
-          const pageLoadTime = + (
-            (navigationTiming.loadEventEnd - navigationTiming.startTime) /
-            1000
-          ).toFixed(4);
-          cy.log(`Total Load Time: ${pageLoadTime} s`);
-          expect(pageLoadTime).to.be.lessThan(TEST_CONFIG.maximumPageLoadTime);
-        }
-      });
-      captureSreenshot();
-    });
-  });
 });
