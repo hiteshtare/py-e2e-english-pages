897138fe730c86d4100b41c3d1f63fa8daf68c11
Hitesh Tare <hiteshtare@gmail.com>
2025-06-30 13:57:02 +0530

    Added test for blogs, events & locations

---
+++ cypress/e2e/priorityLocations.cy.ts
@@ -0,0 +1,27 @@
+// Import common util
+import { TEST_CONFIG } from "./config";
+import { captureSreenshot } from "./util/common.util";
+
+const testDataForItems = require("../fixtures/locations.json");
+
+describe("Locations", () => {
+  testDataForItems.forEach((testCase: any, index: number) => {
+    it(`#${index} page ${testCase.label} should have Page Load Time less than ${TEST_CONFIG.maximumPageLoadTime} seconds`, () => {
+      cy.visit(`${testCase.url}`);
+      cy.window().then((win) => {
+        const [navigationTiming] = win.performance.getEntriesByType(
+          "navigation"
+        ) as PerformanceNavigationTiming[];
+        if (navigationTiming) {
+          const pageLoadTime = +(
+            (navigationTiming.loadEventEnd - navigationTiming.startTime) /
+            1000
+          ).toFixed(4);
+          cy.log(`Total Load Time: ${pageLoadTime} s`);
+          expect(pageLoadTime).to.be.lessThan(TEST_CONFIG.maximumPageLoadTime);
+        }
+      });
+      captureSreenshot();
+    });
+  });
+});
